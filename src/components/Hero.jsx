60c380019c10ea57b41ed533fb3119076406a489
theoutlier <youtu0175@gmail.com>
2025-03-03 05:17:27 +0530

    Update: Cleaned up the working directory

--- src/components/Hero.jsx
+++
@@ -1,9 +0,0 @@
-const Hero = () => {
-    return (
-        <div>
-            <h1>this it a test for the hero section that i am going to create</h1>
-        </div>
-    );
-};
-
-export default Hero;
