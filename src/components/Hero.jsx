52afe6a39bf72917436e8abeda0034fbccbc3eee
theoutlier <youtu0175@gmail.com>
2025-03-17 00:47:46 +0530

    fixed: font weight and hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     return (
         <section className='hero'>
             <button className="button test">COMING SOON</button>
-            <img className="landing_image" src="images/dark-bg.jpg" />
+            <img className="landing_image" src="images/3.png" />
         </section>
     );
 };
