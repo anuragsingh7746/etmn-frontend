4cb8359872ffbf1a999f8f7249950c986bf403ec
theoutlier <youtu0175@gmail.com>
2025-03-16 15:46:58 +0530

    basic layout done

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import '../css/Hero.css'
+
+const Hero = () => {
+
+    return (
+        <section>
+            <button className="button">Test button</button>
+            <img className="landing_image" src="images/dark-bg.jpg" />
+        </section>
+    );
+};
+
+export default Hero;
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   css: {
     preprocessorOptions: {
       // Ensure source maps are enabled for better debugging
